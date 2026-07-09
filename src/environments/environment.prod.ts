11bae4e58f7116ed2921db4b455ace66eccb8576
“JavierEM” <jembfh@gmail.com>
2026-07-09 13:38:50 +0200

    menor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,4 @@ export const environment = {
   production: true,
      supabaseUrl: 'https://urphgdsjpsquoulvcxtf.supabase.co',
      supabaseKey: 'sb_publishable_DK_WXoWxzotDAMMlKL-KzQ_IXdeU6oW',
-
-
-
 };
