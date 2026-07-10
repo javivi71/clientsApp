e3ee8585ee1a5f77e3044f4bde10be09e5f73b3b
“JavierEM” <jembfh@gmail.com>
2026-07-10 09:48:59 +0200

    Eliminar credenciales del repositorio

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-     supabaseUrl: 'https://urphgdsjpsquoulvcxtf.supabase.co',
-     supabaseKey: 'sb_publishable_DK_WXoWxzotDAMMlKL-KzQ_IXdeU6oW',
-};
